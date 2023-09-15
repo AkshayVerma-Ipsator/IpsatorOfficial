bdb525acc1ab4b3c641fe75824f723062a6d8599
Anji@515 <anji.gd17@gmail.com>
2023-09-15 17:57:27 +0530

    Developed teams

--- drain-reactBlog/src/pages/page1/page1.jsx
+++ drain-reactBlog/src/pages/page1/page1.jsx
@@ -1,17 +1,15 @@
-import { Heading } from '@chakra-ui/react';
-import React from 'react';
 import Hero from './hero';
-import Navbar from '../../components/navbar';
-import Header from '../../components/header';
 import Technology from './Technology';
-import DropDown from '../../components/Dropdown';
+import TeamContainer from '../../components/Team';
+import Policies from '../../components/Policies';
 
 const Page1 = () => {
   return (
     <>
     <Hero/>
     <Technology/>
-    <DropDown/>
+    <Policies/>
+    <TeamContainer/>
     </>
   );
 }
