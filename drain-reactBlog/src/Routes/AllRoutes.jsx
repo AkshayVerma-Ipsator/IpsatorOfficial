98f4327c077aeac3735b0f503bcb0e253815024d
saurav903 <sk8517167@gmail.com>
2023-09-15 18:43:19 +0530

    navbar new

--- drain-reactBlog/src/Routes/AllRoutes.jsx
+++ drain-reactBlog/src/Routes/AllRoutes.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Routes,Route} from "react-router-dom"
 import Page1 from '../pages/page1/page1';
+import NewNavbar from '../components/NewNavbar';
 
 const AllRoutes = () => {
   return (
     <Routes>
         <Route path='/' element={<Page1/>}/>
-
+        <Route path='/nav' element={<NewNavbar/>}/>
     </Routes>
 
   )
--- drain-reactBlog/src/pages/page1/hero.jsx
+++ drain-reactBlog/src/pages/page1/hero.jsx
@@ -110,7 +110,7 @@ const Hero = () => {
       <Box boxShadow={"rgba(0, 0, 0, 0.24) 0px 3px 8px;"} py="24px" bgColor="orange.100">
       <Heading 
                     lineHeight={1.1} fontWeight={600} textAlign={"center"} py="24px"
-                    fontSize={"36px"} color="blackAlpha.800" fontWeight="400">
+                    fontSize={"36px"} color="blackAlpha.800" >
                 Highlights of initiatives under WASTE TO WEALTH MISSION
               </Heading>
               <SimpleGrid columns={4} gap="20px"w="90%" m="auto"mt="24px" >
